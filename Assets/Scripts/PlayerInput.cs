e0aea85228fe89a0efa3a1c58312b12d159b788e
Rayelle <equation0703@naver.com>
2024-11-09 10:35:07 +0900

    Prototype Scene Setting

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,11 +4,11 @@
 // 감지된 입력값을 다른 컴포넌트들이 사용할 수 있도록 제공
 public class PlayerInput : MonoBehaviour
 {
-    public string fireButtonName = "Fire1"; // 발사를 위한 입력 버튼 이름
-    public string jumpButtonName = "Jump";
-    public string moveHorizontalAxisName = "Horizontal"; // 좌우 회전을 위한 입력축 이름
-    public string moveVerticalAxisName = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
-    public string reloadButtonName = "Reload"; // 재장전을 위한 입력 버튼 이름
+    public string fireButton = "Fire1"; // 발사를 위한 입력 버튼 이름
+    public string jumpButton = "Jump";
+    public string moveHorizontalAxis = "Horizontal"; // 좌우 회전을 위한 입력축 이름
+    public string moveVerticalAxis = "Vertical"; // 앞뒤 움직임을 위한 입력축 이름
+    public string reloadButton = "Reload"; // 재장전을 위한 입력 버튼 이름
 
     // 값 할당은 내부에서만 가능
 
@@ -16,7 +16,7 @@ public class PlayerInput : MonoBehaviour
     public bool fire { get; private set; } // 감지된 발사 입력값
     public bool reload { get; private set; } // 감지된 재장전 입력값
     public bool jump { get; private set; }
-    
+
 
     // 매프레임 사용자 입력을 감지
     private void Update()
@@ -32,11 +32,11 @@ public class PlayerInput : MonoBehaviour
             return;
         }
 
-        moveInput = new Vector2(Input.GetAxis(moveHorizontalAxisName), Input.GetAxis(moveVerticalAxisName));
+        moveInput = new Vector2(Input.GetAxis(moveHorizontalAxis), Input.GetAxis(moveVerticalAxis));
         if (moveInput.sqrMagnitude > 1) moveInput = moveInput.normalized;
 
-        jump = Input.GetButtonDown(jumpButtonName);
-        fire = Input.GetButton(fireButtonName);
-        reload = Input.GetButtonDown(reloadButtonName);
+        jump = Input.GetButtonDown(jumpButton);
+        fire = Input.GetButton(fireButton);
+        reload = Input.GetButtonDown(reloadButton);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,24 +5,24 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController characterController;
     private PlayerInput playerInput;
     private Animator animator;
-    
+
     private Camera followCam;
-    
+
     public float speed = 6f;
     public float jumpVelocity = 20f;
     [Range(0.01f, 1f)] public float airControlPercent;
 
     public float speedSmoothTime = 0.1f;
     public float turnSmoothTime = 0.1f;
-    
+
     private float speedSmoothVelocity;
     private float turnSmoothVelocity;
-    
+
     private float currentVelocityY;
-    
-    public float currentSpeed =>
-        new Vector2(characterController.velocity.x, characterController.velocity.z).magnitude;
-    
+
+    public float currentSpeed => new Vector2(characterController.velocity.x,
+     characterController.velocity.z).magnitude;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -36,7 +36,7 @@ public class PlayerMovement : MonoBehaviour
         if (currentSpeed > 0.2f || playerInput.fire) Rotate();
 
         Move(playerInput.moveInput);
-        
+
         if (playerInput.jump) Jump();
     }
 
@@ -49,7 +49,7 @@ public class PlayerMovement : MonoBehaviour
     {
         var targetSpeed = speed * moveInput.magnitude;
         var moveDirection = Vector3.Normalize(transform.forward * moveInput.y + transform.right * moveInput.x);
-        
+
         var smoothTime = characterController.isGrounded ? speedSmoothTime : speedSmoothTime / airControlPercent;
 
         targetSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, smoothTime);
