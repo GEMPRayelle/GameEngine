bfa3b6d14084052a86b422979a9784e23e10c9d6
RiuNel <chfhddk1122@gmail.com>
2024-12-11 21:10:22 +0900

    [업데이트] 카메라, 캐릭터 버그 수정, 좀비 제너레이터 길가에 추가
    네비 메시 베이크 추가
    좀비 제너레이터 수정 및 추가
    캐릭터 시네머신 들고옴
    캐릭터 Solider 파일 추가

--- Assets/Scripts/Zombie_Generate.cs
+++ Assets/Scripts/Zombie_Generate.cs
@@ -12,10 +12,13 @@ public class Zombie_Generate : MonoBehaviour
     public HashSet<Vector3> Area = new HashSet<Vector3> {}; // 젠 위치 근처 좀비 랜덤 스폰할 영역
     [SerializeField] private int Generate_Count = 0;
     [SerializeField] private int Max_Generate = 15; //최대 젠 영역 갯수, 만약 조정할거면 Set_Pos에 있는 Random.Range값이 경우의 수가 충분한지 확인 필요
+    Zombie_Generate zombie_Generate;
+    public bool is_generated = false;
     float time = 0.1f; //초기 시간값 건들면 안됨
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        zombie_Generate = GetComponent<Zombie_Generate>();
         if(Player == null)
         {
             Player = GameObject.Find("Player"); //플레이어 빠지는거 방지
@@ -27,11 +30,12 @@ public class Zombie_Generate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Area.Count);
-        if(Vector3.Distance(Player.transform.position,transform.position)<Distance_Generate && Generate_Count < Area.Count) //1. 플레이어와의 거리, 2. 소환횟수가 젠 수를 못벗어나도록 
+        if(Vector3.Distance(Player.transform.position,transform.position)<Distance_Generate && !is_generated) //1. 플레이어와의 거리, 2. 소환횟수가 젠 수를 못벗어나도록 
         {
             StartCoroutine(Coroutine_Generate(time)); //생성코루틴 시작
             time = 20.0f; // 리젠 시간
+            is_generated = true;
+            zombie_Generate.enabled = false;
         }
     }
     void Set_Pos() //랜덤 젠 위치 설정
@@ -52,13 +56,13 @@ public class Zombie_Generate : MonoBehaviour
     IEnumerator Coroutine_Generate(float time = 0.1f) //좀비 생성
     {
         yield return new WaitForSeconds(time);
-        Generate_Count = 0; //리젠을 위해서 초기화
+        
+        //Generate_Count = 0; //리젠을 위해서 초기화
+        
         foreach(var p in Area) //랜덤젠 스폰 위치
         {
             Instantiate(Zombie[Random.Range(0, Zombie.Length - 1)], p, Quaternion.identity); //랜덤으로 좀비 생성
             Generate_Count++; //코루틴 반복횟수 조절용 변수
         }
-        
-        
     }
 }
